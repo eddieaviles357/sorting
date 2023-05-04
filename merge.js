3e5eadc15b1132ec69d10e2225fbf9418bf5a130
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-03 21:17:22 -0700

    merge will merge 2 arrays together until one of the arrays length is 0

--- merge.js
+++ merge.js
@@ -1,4 +1,10 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let sortedArr = [];
+    while(arr1.length && arr2.length) {
+        (arr1[0] > arr2[0]) ? sortedArr.push(arr2.shift()) : sortedArr.push(arr1.shift());
+    }
+    return sortedArr;
+};
 
 function mergeSort() {}
 
