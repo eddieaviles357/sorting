d90a52cbc4bb6fbe00b65577b2d12a7c12093448
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-03 20:55:32 -0700

    bubble sort will compare the value in the array against the next one and swap when necessary until the whole array in sorted, O(n^2) time complexity

--- bubble.js
+++ bubble.js
@@ -1,3 +1,21 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    let temp;
+    let swapped = false;
+
+    for(let i = 0; i < arr.length; i++) {
+        swapped = false; // no swap was done
+
+        for(let j = 0; j < arr.length - 1; j++) {
+            if(arr[ j ] > arr[ j + 1 ]) { // value is greater than next value
+                temp = arr[ j ]; // store in a temp variable
+                arr[ j ] = arr[ j + 1];; // swap value
+                arr[ j + 1 ] = temp; // swap value
+                swapped = true; // a swap was done
+            }
+        };
+        if(!swapped) break; // no swap was done take a break
+    };
+    return arr;
+}
 
 module.exports = bubbleSort;
