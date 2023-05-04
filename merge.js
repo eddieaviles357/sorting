07159b2dc5f4ea2fcac031da7f1ce452b6a4701e
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-04 10:21:25 -0700

    updated merge function to not modify the original array inputs

--- merge.js
+++ merge.js
@@ -1,8 +1,32 @@
+// function merge(arr1, arr2) { // modifies the original array inputs
+//     let sortedArr = [];
+//     while(arr1.length && arr2.length) {
+//         (arr1[0] > arr2[0]) ? sortedArr.push(arr2.shift()) : sortedArr.push(arr1.shift());
+//     }
+//     return sortedArr;
+// };
+// does not modify the original array inputs
 function merge(arr1, arr2) {
     let sortedArr = [];
-    while(arr1.length && arr2.length) {
-        (arr1[0] > arr2[0]) ? sortedArr.push(arr2.shift()) : sortedArr.push(arr1.shift());
-    }
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length) { 
+        if(arr1[i] < arr2[j]) {
+            sortedArr.push(arr1[i]);
+            i++;
+        } else {
+            sortedArr.push(arr2[j]);
+            j++;
+        };
+    };
+    while(i < arr1.length) {
+        sortedArr.push(arr1[i]);
+        i++;
+    };
+    while(j < arr2.length) {
+        sortedArr.push(arr2[j]);
+        j++;
+    };
     return sortedArr;
 };
 
