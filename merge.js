694278c0706d0cd6c66d82f88ac641d0bec02a1a
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-04 20:07:30 -0700

    mergeSort will slice the array into halves recursively and when the arr reaches a length of 1 for both halves mergeSort will use the merge function to compare the values and return a sorted array. This will be repeated until the array is sorted and will be merged

--- merge.js
+++ merge.js
@@ -30,6 +30,12 @@ function merge(arr1, arr2) {
     return sortedArr;
 };
 
-function mergeSort() {}
+function mergeSort(arr) {
+    if(arr.length <= 1) return arr;
+    let middle = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, middle));
+    let right = mergeSort(arr.slice(middle));
+    return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
