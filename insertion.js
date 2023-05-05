61bc6dfc1751fc9def14d40150574a388cf096ce
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-04 20:56:05 -0700

    insertion sort will start at the 2nd element and compare to the left, if the left is larger we will swap and repeat

--- insertion.js
+++ insertion.js
@@ -1,3 +1,13 @@
-function insertionSort() {}
+function insertionSort(arr) {debugger;
+    for (let i = 1; i < arr.length; i++) {
+        let currVal = arr[i]; // used to compare to left
+        let j;
+        for (j = i - 1; j >= 0 && arr[j] > currVal; j--) {
+          arr[j + 1] = arr[j]; // assign left side to right side
+        }
+        arr[j + 1] = currVal; // assign to current value
+      }
+      return arr;
+}
 
 module.exports = insertionSort;
